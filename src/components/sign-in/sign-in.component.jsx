62195e3118b8642673b18711a6b80582874414d1
Yacoub Abdulla <Yacoub.Abdulla@ibm.com>
2020-09-04 12:04:31 -0400

    implemented firebase utils, included ability to store authenticated users into our fire store database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+
+
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component'
+
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
-
-
-import FormInput from '../form-input/form-input.component'
-import CustomButton from '../custom-buttom/custom-buttom.component'
-
-import { signInWithGoogle } from '../../firebase/firebase.utils'
-
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -16,19 +16,20 @@ class SignIn extends React.Component {
         this.state = {
             email: '',
             password: ''
-        }
+        };
     }
 
     handleSubmit = event => {
-        event.preventDafault();
-        this.setState({ email: '', password: '' })
-    }
+        event.preventDefault();
+
+        this.setState({ email: '', password: '' });
+    };
 
     handleChange = event => {
-        const { value, name } = event.target
+        const { value, name } = event.target;
 
         this.setState({ [name]: value });
-    }
+    };
 
     render() {
         return (
@@ -40,31 +41,29 @@ class SignIn extends React.Component {
                     <FormInput
                         name='email'
                         type='email'
-                        value={this.state.email}
                         handleChange={this.handleChange}
+                        value={this.state.email}
                         label='email'
-                        required />
-
+                        required
+                    />
                     <FormInput
                         name='password'
                         type='password'
                         value={this.state.password}
                         handleChange={this.handleChange}
                         label='password'
-                        required />
-
-                    <div className='button'>
-                        <CustomButton type="submit"> Sign in </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google </CustomButton>
+                        required
+                    />
+                    <div className='buttons'>
+                        <CustomButton type='submit'> Sign in </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            Sign in with Google
+            </CustomButton>
                     </div>
-
-
-
                 </form>
-
             </div>
-        )
+        );
     }
 }
 
-export default SignIn
+export default SignIn;
